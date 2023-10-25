93e3c786a7448996d7a6fae4af9722bef299f52c
Paul Menefee <paul.menefee@gmail.com>
2023-10-25 09:59:03 -0400

    init commit

---
+++ scenes/main.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public partial class main : Node3D
+{
+}
---
+++ scenes/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody3D
+
+@onready var armature = $Armature
+@onready var spring_arm_pivot = $SpringArmPivot
+@onready var spring_arm = $SpringArmPivot/SpringArm3D
+@onready var animation_tree = $AnimationTree
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const LERP_VALUE = .15
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+	if event is InputEventMouseMotion:
+		spring_arm_pivot.rotate_y(-event.relative.x * .005)
+		spring_arm.rotate_x(-event.relative.y * .005)
+		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
+	if direction:
+		velocity.x = lerp(velocity.x, direction.x * SPEED, LERP_VALUE)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_VALUE)
+		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VALUE)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, LERP_VALUE)
+		velocity.z = lerp(velocity.z, 0.0, LERP_VALUE)
+
+	animation_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() /SPEED)
+
+	move_and_slide()
